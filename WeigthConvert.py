5c101bc19d90e517ae558c83727fef5fcaa16227
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-08-15 17:01:33 +0530

    Conversion area clears after button press

--- WeigthConvert.py
+++ WeigthConvert.py
@@ -6,21 +6,19 @@ def kgconvert():
     pounds = str(float(val.get())*2.20462) + " pounds"
     ounces = str(float(val.get())*35.274) + " ounces"
 
+    t1.delete(1.0,END)
     t1.insert(END, grams)
+
+    t2.delete(1.0,END)
     t2.insert(END,pounds)
+
+    t3.delete(1.0,END)
     t3.insert(END,ounces)
 
-m1=Message(window, text="Kg")
-m1.grid(row=0, column=0)
+window.geometry("450x100");
 
-# l1=Label(window, text="grams")
-# l1.grid(row=1,column=1)
-#
-# l2=Label(window, text="pounds")
-# l2.grid(row=1,column=3)
-#
-# l3=Label(window, text="ounces")
-# l3.grid(row=1,column=5)
+m1=Message(window, text="Weight(in kg)")
+m1.grid(row=0, column=0, sticky=W)
 
 val = StringVar()
 e1 = Entry(window, textvariable=val)
@@ -29,13 +27,13 @@ e1.grid(row=0, column=1)
 b1 = Button(window,text="Convert",command=kgconvert)
 b1.grid(row=0, column=2)
 
-t1=Text(window,height=1,width=20)
+t1=Text(window,height=2,width=20)
 t1.grid(row=1,column=0)
 
-t2=Text(window,height=1,width=20)
+t2=Text(window,height=2,width=20)
 t2.grid(row=1,column=1)
 
-t3=Text(window,height=1,width=20)
+t3=Text(window,height=2,width=20)
 t3.grid(row=1,column=2)
 
 
