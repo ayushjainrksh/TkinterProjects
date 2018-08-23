99bc6547d247cf9f5b1aced7ed8b576eee5121f5
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-08-23 22:48:52 +0530

    insert, view and search added on the front end

--- Bookstore/bookstore.py
+++ Bookstore/bookstore.py
@@ -1,4 +1,23 @@
 from tkinter import *
+import backend
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        list1.insert(END, row)
+
+def insert_command():
+    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    list1.delete(0,END)
+    list1.insert(END,title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+
+# def update_command():
+    # backend.update()
 
 window = Tk()
 
@@ -11,27 +30,27 @@ l3.grid(row=1,column=0)
 l4=Label(window,text="ISBN")
 l4.grid(row=1,column=2)
 
-title = StringVar()
-t1 = Entry(window,textvariable=title)
+title_text = StringVar()
+t1 = Entry(window,textvariable=title_text)
 t1.grid(row=0, column=1)
 
-author = StringVar()
-t2 = Entry(window,textvariable=author)
+author_text = StringVar()
+t2 = Entry(window,textvariable=author_text)
 t2.grid(row=0, column=3)
 
-year = StringVar()
-t3 = Entry(window,textvariable=year)
+year_text = StringVar()
+t3 = Entry(window,textvariable=year_text)
 t3.grid(row=1, column=1)
 
-isbn = StringVar()
-t4 = Entry(window,textvariable=isbn)
+isbn_text = StringVar()
+t4 = Entry(window,textvariable=isbn_text)
 t4.grid(row=1, column=3)
 
-b1 = Button(text="View all", width=12)
+b1 = Button(text="View all", width=12, command=view_command)
 b1.grid(row=2, column=3)
-b2 = Button(text="Search entry", width=12)
+b2 = Button(text="Search entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
-b3 = Button(text="Add entry", width=12)
+b3 = Button(text="Add entry", width=12, command=insert_command)
 b3.grid(row=4, column=3)
 b4 = Button(text="Update", width=12)
 b4.grid(row=5, column=3)
