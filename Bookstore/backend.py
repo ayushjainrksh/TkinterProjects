a2ffb2ea5d97539b29382cf9d39ede99973ef778
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-08-23 21:45:17 +0530

    insert and view functions added

--- Bookstore/backend.py
+++ Bookstore/backend.py
@@ -6,3 +6,21 @@ def connect:
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     conn.commit()
     conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("book.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+connect()
