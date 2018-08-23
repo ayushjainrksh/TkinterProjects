8fd4a5e6866b1453e19bec1cfb512ff0a29fc937
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-08-23 21:56:31 +0530

    Search function added

--- Bookstore/backend.py
+++ Bookstore/backend.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def connect:
+def connect():
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
@@ -15,12 +15,22 @@ def insert(title, author, year, isbn):
     conn.close()
 
 def view():
-    conn = sqlite3.connect("book.db")
+    conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM book")
     rows = cur.fetchall()
     conn.close()
     return rows
 
+def search(title="", author="", year="", isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book where title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
 
 connect()
+# insert("The Sea","John Tablet",1918,912554422)
+print(view())
